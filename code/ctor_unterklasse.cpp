e6dd29f8b76455dee180f8fee951309f529825f8
Iceteavanill <faebu.steiner@hotmail.com>
2024-05-27 18:36:50 +0200

    Minimal changes for readability

--- code/ctor_unterklasse.cpp
+++ code/ctor_unterklasse.cpp
@@ -1,6 +1,5 @@
 Unterklasse::unterklasse(int _initvalOberklasse, 
                          int _initvallUnterklasse):
-    Oberklasse{_initvalOberklasse},
+    Oberklasse{_initvalOberklasse},// muss zuerst stehen da diese zuerst initialisiert werden muss
     unterklassenAttribut{_initvallUnterklasse}
-{
-}
+{} // bleibt leer
